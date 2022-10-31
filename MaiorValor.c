a3255ec32b49dfd3211b5973ae275115f3d75169
Yasmin Devegili <48127784+yasmindevegili@users.noreply.github.com>
2022-10-31 19:22:25 -0300

    Update MaiorValor.c

--- MaiorValor.c
+++ MaiorValor.c
@@ -5,39 +5,48 @@ int main()
     int a, b, c;
     scanf("%i %i %i", &a, &b, &c);
     
+    int Maior(){    
     if(a>b && a>c){
-        printf("%i", a);
+        printf("%i\n", a);
         if(b>c){
-            printf("%i",b);
-            printf("%i",c);
+            printf("%i\n",b);
+            printf("%i\n",c);
+            printf("\n%i\n%i\n%i", c, b, a);
         }
         else{
-            printf("%i",c);
-            printf("%i",b);
+            printf("%i\n",c);
+            printf("%i\n",b);
+            printf("\n%i\n%i\n%i", b, c, a);
         }
     }
     else if(b>c && b>a){
-        printf("%i",b);
+        printf("%i\n",b);
         if(a>c){
-            printf("%i",a);
-            printf("%i",c);
+            printf("%i\n",a);
+            printf("%i\n",c);
+            printf("\n%i\n%i\n%i", c, a, b);
         }
         else{
-            printf("%i",c);
-            printf("%i",a);
+            printf("%i\n",c);
+            printf("%i\n",a);
+            printf("\n%i\n%i\n%i", a, c, b);
         }
     }
     else{
-        printf("%i",c);
+        printf("%i\n",c);
         if(b>a){
-            printf("%i",b);
-            printf("%i",a);
+            printf("%i\n",b);
+            printf("%i\n",a);
+            printf("\n%i\n%i\n%i", a, b, c);
         }
         else{
-            printf("%i",a);
-            printf("%i",b);
+            printf("%i\n",a);
+            printf("%i\n",b);
+            printf("\n%i\n%i\n%i", b, a, c);
         }
        
     }
+    }
+    Maior();
     return 0;
 }
